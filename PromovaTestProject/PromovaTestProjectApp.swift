ff37030bd1e99880c9ac7c00a80d8db70dfac8a5
Oleksandr Kovalov <oleksandr.k19251@volpis.com>
2024-11-12 15:38:19 +0200

    Initial Commit

---
+++ PromovaTestProject/PromovaTestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PromovaTestProjectApp.swift
+//  PromovaTestProject
+//
+//  Created by Alex Kovalov on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct PromovaTestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PromovaTestProjectTests/PromovaTestProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  PromovaTestProjectTests.swift
+//  PromovaTestProjectTests
+//
+//  Created by Alex Kovalov on 11/12/24.
+//
+
+import Testing
+@testable import PromovaTestProject
+
+struct PromovaTestProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
